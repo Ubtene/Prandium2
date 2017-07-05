fbb50e2cf287a6a41519f86632799ab21b679f82
fodnb <Daniel.Mcghee6813@gmail.com>
2017-07-04 21:43:15 -0400

    simplified day.js

--- app/components/children/Day.js
+++ app/components/children/Day.js
@@ -2,45 +2,26 @@ import React from 'react';
 
 export const Day = (props) => {
     return (
-    <div className="day-wrapper panel">
+    <div className="panel" id="day-wrapper">
+
       <div className="day-name">
-        Day:
         {props.day}
       </div>
+
+
       <div className="meals">
-        <div className="breakfast">
-          Breakfast:
-          {props.breakfast}
-          <button className="">Delete Meal</button>
-        </div>
+
         <div className="lunch">
-          Lunch:
           {props.lunch}
-          <button className="">Delete Meal</button>
+        {/*  <button className="">Delete Meal</button>*/}
         </div>
-        <div className="dinner">
-          Dinner:
-          {props.dinner}
-          <button className="">Delete Meal</button>
-        </div>
+
       </div>
-      <div className="ingredients">
-        <div className="ingredients-names">
-          Ingredients Needed:
-          {props.ingredients}
-        </div>
-        <img
-          className="ingredients-img"
-          src=""
-          alt="picture of ingredients"
-        />
-      </div>
-      <div className="instructions">
-        Instructions:
-        {props.instructions}
-      </div>
+
+
+
       <img className="meal-img" alt="Image of meal" src="" />
-      <button>Update Meals</button>
+     {/* <button>Update Meals</button>*/}
     </div>
   );
 }
