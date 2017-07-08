52bec7aa960b272e4fefad1439ce1d96b8b14bd1
Juan <whoisju1@gmail.com>
2017-07-07 23:17:31 -0400

    added button

--- app/components/children/Day.js
+++ app/components/children/Day.js
@@ -1,17 +1,29 @@
 import React from "react";
-import { deleteMeals } from "../utils/helpers";
+import { deleteMeals, updateMeals } from "../utils/helpers";
 
 export const Day = props => {
-  const handleClick = () => {
-    let day = props.day;
+  
+  const handleDelete = () => {
+ let day = props.day;
     let user = props.user.data["0"].userID;
     console.log("DAY:", day);
     console.log("USER:", user);
     deleteMeals(user, day);
-  };
+    console.log('dlt clicked');
+
+  }; 
+  const handleUpdate = () => {
+     let day = props.day;
+    let user = props.user.data["0"].userID;
+    console.log("DAY:", day);
+    console.log("USER:", user);
+    updateMeals(user, day);
+    console.log('update clicked');
+  }; 
 
   return (
     <div className="day-wrapper">
+      <div className='delete-meal-btn' onClick={handleDelete}><i className="fa fa-times" aria-hidden="true"></i></div>
     
       <div className="day-name">
         {props.day}
@@ -20,8 +32,8 @@ export const Day = props => {
           {props.meal}
       </div>
 
-      <div className='refresh-meal-btn'><i className="fa fa-refresh" aria-hidden="true"></i></div>
       <img className="meal-img" alt="Image of meal" src={props.image} />
+      <div className='refresh-meal-btn' onClick={handleUpdate}><i className="fa fa-refresh" aria-hidden="true"></i></div>
     </div>
   );
 };
--- app/components/utils/helpers.js
+++ app/components/utils/helpers.js
@@ -34,8 +34,15 @@ var helper = {
   },
 
 
-  deleteMeals(user, day) {
+  updateMeals(user, day) {
 	  return axios.post('/update/', {user: user, day: day})
+	  .then(data=> {
+		  console.log('meals to delete', data);
+		  return data;
+	  }); 
+	},
+  deleteMeals(user, day) {
+	  return axios.post('/delete/', {user: user, day: day})
 	  .then(data=> {
 		  console.log('meals to delete', data);
 		  return data;
